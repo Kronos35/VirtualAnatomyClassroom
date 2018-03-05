b112b4901b565685cbd1d7e0b8b7c1b0a2419555
Kronos35 <xxchronos35xx@gmail.com>
2018-03-05 15:25:05 -0700

    Initial commit

---
+++ app/Http/Controllers/TissueController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tissue;
+use Illuminate\Http\Request;
+
+class TissueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tissue  $tissue
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tissue $tissue)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tissue  $tissue
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tissue $tissue)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tissue  $tissue
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tissue $tissue)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tissue  $tissue
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tissue $tissue)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_03_05_214842_create_tissues_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTissuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tissues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('tissue_type_id');
+            $table->text('content');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tissues');
+    }
+}
