64c4eef182b7a28afb01b50b4298d1155bd6471b
Kronos35 <xxchronos35xx@gmail.com>
2018-05-14 18:21:17 -0600

    Adding proper js reference

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -159,8 +159,8 @@
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Plugin JavaScript -->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
