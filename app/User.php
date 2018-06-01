e73fc79cc5d26d0f8fe02e438502633dc903bbb4
Kronos35 <xxchronos35xx@gmail.com>
2018-05-31 20:37:48 -0600

    adding roles and permissions to users

--- app/User.php
+++ app/User.php
@@ -4,11 +4,12 @@ namespace App;
 
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,11 +15,61 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Roles
+        $admin = Role::create(['name' => 'admin']);
+        $writer = Role::create(['name' => 'writer']);
+        $student = Role::create(['name' => 'student']);
+        $teacher = Role::create(['name' => 'teacher']);
+
+        // Permissions
+        $permit_ban_users = Permission::create(['name' => 'ban users']);
+        $permit_ban_articles = Permission::create(['name' => 'ban articles']);
+        $permit_edit_articles = Permission::create(['name' => 'edit articles']);
+        $permit_create_articles = Permission::create(['name' => 'create articles']);
+        $permit_read = Permission::create(['name' => 'read articles']);
+        
+        // Permission assignment
+        $admin->givePermissionTo($permit_ban_users);
+        $admin->givePermissionTo($permit_ban_articles);
+        $admin->givePermissionTo($permit_edit_articles);
+        $admin->givePermissionTo($permit_create_articles);
+        $admin->givePermissionTo($permit_read);
+
+        $writer->givePermissionTo($permit_ban_articles);
+        $writer->givePermissionTo($permit_create_articles);
+        $writer->givePermissionTo($permit_read);
+
+        $student->givePermissionTo($permit_read);
+        $teacher->givePermissionTo($permit_read);
+
+        // Create user potato
         DB::table('users')->insert([
-            'name' => 'Potato',
+            'name' => 'Admin',
             'email' => 'potato@potato.com',
             'password' => bcrypt('123456'),
         ]);
+
+        // Create user potato
+        DB::table('users')->insert([
+            'name' => 'student',
+            'email' => 'student@potato.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        // Create user potato
+        DB::table('users')->insert([
+            'name' => 'Teacher',
+            'email' => 'teacher@potato.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        $potato = User::where('name', 'Admin')->first();
+        $potato->assignRole('writer');
+
+        $potato = User::where('name', 'Student')->first();
+        $potato->assignRole('student');
+
+        $potato = User::where('name', 'Teacher')->first();
+        $potato->assignRole('teacher');
     }
 }
